c3c1ea882486a0e44ec8961f74c1242a628318f8
Parkboyoung11 <vuhongsonjv11@gmail.com>
2018-07-20 08:25:03 +0700

    Done

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,8 +3,10 @@ module UsersHelper
     size = options[:size]
     if File.exist?(Rails.root.to_s + "/app/assets/images/#{user.id}.jpg")
       gravatar_url = "#{user.id}.jpg"
+      # gravatar_url = "https://goo.gl/38qDm2"
     else
       gravatar_url = "default.png"
+      # gravatar_url = "https://goo.gl/hGxXPr"
     end
     image_tag(gravatar_url, alt: user.name, class: "gravatar", size: size)
   end
